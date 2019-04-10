da6e1896fa90e9c25c8e8c48dd7eafa84b6879b2
elisabeth.sugiarto <elisabeth.sugiarto@gdn-commerce.com>
2019-04-10 22:09:00 +0700

    [BAFE-8] Remove unrelated feature

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../pages/Home.vue'
-import AboutUs from '../pages/AboutUs.vue'
-import DetailArtikel from '../pages/DetailArtikel.vue'
-import UploadArtikel from '../pages/UploadArtikel.vue'
-import TheLogin from '../pages/TheLogin.vue'
-import Konten from '../pages/Konten.vue'
-import TermsAndCondition from '../pages/TermsAndCondition.vue'
+import Login from '../pages/Login.vue'
 
 Vue.use(Router)
 
@@ -14,39 +8,9 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/About',
-      name: 'aboutUs',
-      component: AboutUs
-    },
-    {
-      path: '/DetailArtikel',
-      name: 'detailArtikel',
-      component: DetailArtikel
-    },
-    {
-      path: '/UploadArtikel',
-      name: 'uploadArtikel',
-      component: UploadArtikel
-    },
-    {
-      path: '/pintukemanaajadoraemon',
-      name: 'theLogin',
-      component: TheLogin
-    },
-    {
-      path: '/Konten',
-      name: 'konten',
-      component: Konten
-    },
-    {
-      path: '/TermsAndCondition',
-      name: 'TAC',
-      component: TermsAndCondition
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
