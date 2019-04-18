5e76dc2dae15f61b3330bcee0cc19692b40cdad4
Andhika Satria <andhikasatriab@gmail.com>
2019-04-18 22:55:04 +0700

    [BAFE-5]add all

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '../pages/home.vue'
 import aboutUs from '../pages/aboutUs.vue'
-import uploadArtikel from '../pages/uploadArtikel.vue'
-import theLogin from '../pages/theLogin.vue'
-import konten from '../pages/konten.vue'
 
 Vue.use(Router)
 
@@ -12,29 +8,9 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: home
-    },
-    {
       path: '/about',
       name: 'aboutUs',
       component: aboutUs
-    },
-    {
-      path: '/uploadArtikel',
-      name: 'uploadArtikel',
-      component: uploadArtikel
-    },
-    {
-      path: '/pintukemanaajadoraemon',
-      name: 'theLogin',
-      component: theLogin
-    },
-    {
-      path: '/konten',
-      name: 'konten',
-      component: konten
     }
   ]
 })
