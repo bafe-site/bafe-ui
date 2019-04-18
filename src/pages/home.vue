50611c23ea243465f5886545aeea30cfa0fbb87b
Elisabeth Diana K S <elisabeth.dianaks@gmail.com>
2019-04-18 23:29:39 +0700

    [BAFE-3] Fix merge with release

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,170 +1,204 @@
 <template>
-    <div>
-        <div>
-            <h1 class="headVision">Citizen Journalism Platform for Innovative Solution</h1>
-        </div>
-        <div class="howTo">
-            <div class="howToW">
-                <p>bergabunglah bersama kami untuk...</p>
-                <h2>Share ur Innovation</h2>
-            </div>
-            <input onclick="location.href='/termsAndCondition';" class="uploadButton" type="button" value="SUBMIT">
-        </div>
-        <div class="misi">
-            <h2 style="text-align: center;">Why Sharing?</h2>
-            <img class="gambarTengah" align="left" src="../assets/S__17219904.jpg">
-            <p class="tulisanMisi">Vue.js uses an HTML-based template syntax that allows you to declaratively bind the rendered DOM to the
-                underlying Vue instance’s data. All Vue.js templates are valid HTML that can be parsed by spec-compliant
-                browsers and HTML parsers. Under the hood, Vue compiles the templates into Virtual DOM render functions.
-                Combined with the reactivity system, Vue is able to intelligently figure out the minimal number of
-                components to re-render and apply the minimal amount of DOM manipulations when the app state changes.
-                </p>
-        </div>
-        <div>
-            <h1>Konten Teratas</h1>
-        </div>
-        <div class="topKonten">
-            <span>bafe.id</span>
-            <img class="gambarTop" align="left" src="../assets/S__17219904.jpg">
-            <p class="artikelTop">Vue.js uses an HTML-based template syntax that allows you to declaratively bind the rendered DOM to the
-                underlying Vue instance’s data. All Vue.js templates are valid HTML that can be parsed by spec-compliant
-                browsers and HTML parsers. Under the hood, Vue compiles the templates into Virtual DOM render functions.
-                Combined with the reactivity system, Vue is able to intelligently figure out the minimal number of
-                components to re-render and apply the minimal amount of DOM manipulations when the app state changes.
-                Vue.js uses an HTML-based template syntax that allows you to declaratively bind the rendered DOM to the
-                underlying Vue instance’s data. All Vue.js templates are valid HTML that can be parsed by spec-compliant
-                browsers and HTML parsers. Under the hood, Vue compiles the templates into Virtual DOM render functions.</p>
-        </div>
-        <div>
-            <h1>Konten Terbaru</h1>
-        </div>
-        <div class="artikelLatestKontainer">
-            <div class="grid-item">
-                <a class="artikel_te" href="/detailArtikel">
-                <img class="gambarLatest" align="left" src="../assets/S__17219904.jpg"></a>
-                <div>
-                    <h3>bafe.id</h3>
-                </div>
-            </div>
-            <div class="grid-item">
-                <a class="artikel_te" href="/detailArtikel">
-                <img class="gambarLatest" align="left" src="../assets/S__17219904.jpg">
-                </a>
-                <div>
-                    <h3>bafe.id</h3>
-                </div>
-            </div>
-            <div class="grid-item">
-                <a class="artikel_te" href="/detailArtikel">
-                <img class="gambarLatest" align="left" src="../assets/S__17219904.jpg">
-                </a>
-                <div>
-                    <h3>bafe.id</h3>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="motto">
+        <h1>Citizen Journalism Platform for Innovative Solution</h1>
+      </div>
     </div>
+    <div class="row">
+      <the-banner
+        :dataset="banner.dataset"
+        :config="banner.config"></the-banner>
+    </div>
+    <div class="row">
+      <div class="item item__summary--horizontal">
+        <div class="summary__thumbnail">
+          <img align="left" src="../assets/img/S__17219904.jpg">
+        </div>
+        <div class="summary__content">
+          <div class="set__title content__title">
+            <h2>Why Sharing?</h2>
+          </div>
+          <div class="content__description">
+            <p>Vue.js uses an HTML-based template syntax that allows you to declaratively bind the rendered DOM to the
+              underlying Vue instance’s data. All Vue.js templates are valid HTML that can be parsed by spec-compliant
+              browsers and HTML parsers. Under the hood, Vue compiles the templates into Virtual DOM render functions.
+              Combined with the reactivity system, Vue is able to intelligently figure out the minimal number of
+              components to re-render and apply the minimal amount of DOM manipulations when the app state changes.
+              Vue.js uses an HTML-based template syntax that allows you to declaratively bind the rendered DOM to the
+              underlying Vue instance’s data.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" ref="carousel">
+      <h2>Top Content</h2>
+      <the-carousel
+        :dataset="carousel.top.dataset"
+        :config="carousel.top.config"></the-carousel>
+    </div>
+    <div class="row">
+      <div class="grid-container">
+        <div class="grid-item" v-for="n in latestContent" :key="n.id">
+          <div class="item item__summary--vertical">
+            <div class="summary__thumbnail">
+              <img v-if="n.thumbnail" :src="require('../assets/img/' + n.thumbnail)" :alt="n.thumbnail">
+            </div>
+            <div class="summary__content">
+              <div class="content__title">
+                <h3> {{ n.title }} </h3>
+              </div>
+              <div class="content__description">
+                <p>{{ n.content | truncate(36) }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import TheCarousel from '../components/TheCarousel'
+import TheBanner from '../components/TheBanner'
 export default {
-  name: 'home'
+  name: 'Home',
+  components: {TheCarousel, TheBanner},
+  data () {
+    return {
+      latestContent: [],
+      carousel: {
+        top: {
+          config: {
+            max: 0
+          },
+          dataset: {
+            articles: [{
+              title: '',
+              content: '',
+              thumbnail: ''
+            }]
+          }
+        }
+      },
+      banner: {
+        config: {},
+        dataset: {
+          contents: [{
+            title: '',
+            description: '',
+            background: '',
+            action: {
+              text: '',
+              link: ''
+            },
+            style: {
+              align: '',
+              mode: 0
+            }
+          }]
+        }
+      }
+    }
+  },
+  mounted () {
+    let self = this
+    self.$nextTick(() => {
+      self.init()
+    })
+  },
+  methods: {
+    init () {
+      let self = this
+      axios.get('http://localhost/bafe/public/api/article?orderBy=id&direction=desc')
+        .then(response => {
+          self.carousel.top.dataset.articles = response.data.content.data.splice(0, 3)
+        })
+        .catch(error => {
+          alert(error)
+        })
+
+      axios.get('http://localhost/bafe/public/api/article?orderBy=viewer&direction=desc')
+        .then(response => {
+          self.latestContent = response.data.content.data.splice(0, 4)
+        })
+        .catch(error => {
+          alert(error)
+        })
+
+      self.banner.dataset.contents = [{
+        title: 'Share Your Innovation',
+        description: 'Bergabunglah bersama kami untuk...',
+        background: 'orang_ramai.jpg',
+        action: {
+          text: 'Upload',
+          link: ''
+        },
+        style: {
+          align: '',
+          mode: 1
+        }
+      }, {
+        title: 'Ceritakan Kisah Anda',
+        description: 'Ceritakan isu-isu yang terjadi disekitarmu, masalah seperti apa yang dicari?',
+        background: 'gambar_isi.jpg',
+        action: {
+          text: 'Tulis Sekarang',
+          link: ''
+        },
+        style: {
+          align: '',
+          mode: 0
+        }
+      }]
+    }
+  }
 }
 </script>
 
-<style scoped>
-    .headVision {
-        text-align: center;
-        color: #385446;
+<style lang="scss" scoped>
+  .row {
+    margin: 50px 0px;
+  }
+  .motto {
+    text-align: center;
+    color: #385446;
+  }
+
+  .container {
+    padding: 0px 10vw;
+  }
+
+  .summary {
+    &__thumbnail {
+      & > img {
+        max-width: 100%;
+        max-height: 100%;
+      }
     }
-    h1 {
-        text-align: center;
+    &__content {
+      box-sizing: border-box;
     }
-    h3 {
-        color: #385446;
-        text-align: center;
+  }
+
+  .grid-container {
+    display: grid;
+    margin: 30px 0px;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    background-color: #385446;
+    color: #fff;
+    padding: 10px 20px
+  }
+
+  .grid-item {
+    padding: 10px 10px;
+    text-align: center;
+
+    &:hover {
+      background-color: #1c261d;
+      -webkit-transition: 100ms;
     }
-    a {
-        color: #385446;
-        text-decoration: none;
-    }
-    .howTo {
-        display: grid;
-        grid-template-columns: auto;
-        margin: 0 auto;
-        height: 450px;
-        color: #fafafa;
-        background-color: #385446;
-        background-image: url('../assets/orang_ramai.jpg');
-        background-position: 50% 0px;
-        background-repeat: no-repeat;
-    }
-    .howToW {
-        margin: 10px auto;
-        text-align: center;
-    }
-    .uploadButton {
-        margin: 0 auto;
-        width: 150px;
-        height: 70px;
-        float: center;
-        padding: 15px 30px;
-        border: 3px solid #fafafa;
-        border-radius: 30px;
-        background-color: #385446;
-        color: #fafafa;
-        font-size: 16px;
-    }
-    .misi {
-        margin: 0 auto;
-        width: 75%;
-        height: 250px;
-        padding: 20px 40px;
-        text-align: justify;
-        border: 0px;
-        color: #000000;
-    }
-    .tulisanMisi {
-        border: 1px;
-        margin: 30px;
-    }
-    .gambarTengah {
-        height: 200px;
-        width: 200px;
-        padding: 5px;
-    }
-    .topKonten {
-        margin: 0 auto;
-        width: 60%;
-        max-height: 250px;
-        padding: 20px 40px;
-        text-align: justify;
-        background-image: url("../assets/orang_jalan.jpg");
-        }
-    .artikelTop {
-        color: #ffffff;
-        padding: 5px;
-    }
-    .gambarTop {
-        max-height: 200px;
-        max-width: 300px;
-        padding-right: 5px;
-    }
-    .artikelLatestKontainer {
-        display: grid;
-        grid-template-columns: auto auto auto;
-        background-color: #fafafa;
-    }
-    .grid-item {
-        display: grid;
-        grid-template-columns: auto;
-        border: 20px solid #ffffff;
-        padding: 20px;
-        font-size: 18px;
-        height: 300px;
-    }
-    .gambarLatest {
-        max-height: 200px;
-        max-width: 300px;
-    }
+  }
 </style>
