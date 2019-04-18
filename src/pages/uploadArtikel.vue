ef20e3020f32ea140218adb84a4b727afde87952
Andhika Satria <andhikasatriab@gmail.com>
2019-04-18 22:36:10 +0700

    [BAFE-4] upload API, Add feature

--- src/pages/uploadArtikel.vue
+++ src/pages/uploadArtikel.vue
@@ -9,11 +9,29 @@
               <vue-editor onfocus="this.value=''" v-model="content"></vue-editor>
             </div>
         </div>
-        <div class="tagArea">
-          <h2>Kategori</h2>
-          <select v-model="kategoriD">
-            <option v-for="n in kategori" :key="n.id">{{ n.categoryName }}</option>
-          </select>
+          <div>
+            <div>
+              <h2>Author</h2>
+              <input v-model="author" type="text">
+            </div>
+            <div class="tagArea">
+              <h2>Kategori</h2>
+              <select v-model="kategoriD">
+                <option v-for="n in kategori" :key="n.id" v-bind:value="n.id">{{ n.categoryName }}</option>
+              </select>
+            <div>
+              <h2>Tag</h2>
+              <input type="text" v-model="tag[0]">
+              <input type="text" v-model="tag[1]">
+              <input type="text" v-model="tag[2]">
+            </div>
+          </div>
+          <div class="uploadArea">
+            <h2>Upload Gambar</h2>
+            <input type="file" @change="processImage($event)">
+            <h2>Upload Video</h2>
+            <input type="file" @change="processVideo($event)">
+          </div>
         </div>
       </div>
       <div class="tUploadArtikel">
@@ -35,24 +53,48 @@ export default {
       judulContent: 'Tulis Judul Artikelmu disini ...',
       content: '',
       kategori: [],
-      kategoriD: ''
+      kategoriD: '',
+      thumbnail: '',
+      video: '',
+      author: '',
+      tag: []
     }
   },
   methods: {
     saveContent () {
+      var kategoriID = parseInt(this.kategoriD)
+      var tagS = this.tag.join()
+      let data = JSON.stringify({
+        title: this.judulContent,
+        content: this.content,
+        category: kategoriID,
+        thumbnail: this.thumbnail,
+        video: this.video,
+        author: this.author,
+        tag: tagS
+      })
       Axios
-        .post('http://localhost/bafe/public/api/article', {
-          title: this.judulContent,
-          content: this.content,
-          categoryCodes: this.kategoriD
-        }, {
+        .post('http://localhost/bafe/public/api/article', data, {
           headers: {
-            Authorization: 'Bearer' + this.$cookie.get('token')
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + this.$cookie.get('token'),
+            'Accept': 'application/json'
           }
         })
         .then(res => {
-          console.log(this.$cookie.get('token'))
+          console.log(data)
+          console.log(typeof (kategoriD))
         })
+        .catch(err => {
+          console.log(data)
+          console.log(err)
+        })
+    },
+    processImage (event) {
+      this.thumbnail = event.target.files[0].name
+    },
+    processVideo (event) {
+      this.video = event.target.files[0].name
     }
   },
   mounted () {
