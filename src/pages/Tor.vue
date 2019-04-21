0143f8788d817e21d63e6ea2f6410e0ceff49326
Andhika Satria Bagaskoro <andhikasatriab@gmail.com>
2019-04-21 23:10:39 +0700

    [BAFE-14] fix content

--- src/pages/Tor.vue
+++ src/pages/Tor.vue
@@ -6,7 +6,7 @@
         <li>
           Syarat unggah artikel
           <ul>
-            <li>Artikel diunggah dengan menggunakan kaidah bahasa indonesia yang baik dan benar.</li>
+            <li>Artikel diunggah dengan menggunakan kaidah Bahasa Indonesia yang baik dan benar.</li>
             <li>Artikel diunggah dalam format .docx.</li>
             <li>Artikel diunggah dengan minimal 750 kata.</li>
             <li>Foto yang diunggah di dalam artikel maksimal ukuran file 3MB.</li>
@@ -25,7 +25,7 @@
             <li> Video yang diunggah tidak memuat unsur Politik Praktis.</li>
           </ul>
         </li>
-        <li>Segala bentuk artikel atau video yang dipublikasikan di situs bafe.site tidak diperkenankan untuk dipergunakan kembali di situs selain bafe.site.</li>
+        <li>Segala bentuk artikel atau video yang dipublikasikan di situs bafe.site tidak diperkenankan untuk dipergunakan kembali di situs selain bafe.site untuk tujuan komersil.</li>
         <li>Segala isi dari konten yang dipublikasikan oleh kontributor menjadi tanggung jawab dari kontributor tersebut.</li>
         <li>Jika memakai foto ilustrasi diharapkan mencantumkan sumber.</li>
         <li>Harap membaca dengan baik <em>disclaimer</em> dan Pedoman Media Siber yang ada pada bafe.site.</li>
@@ -33,8 +33,8 @@
       </ol>
       <input id="acceptTOR" type="checkbox" v-model="checked">
       <label for="acceptTOR">Dengan ini saya telah menyetujui peraturan yang ada.</label>
-      <input v-if="checked" onclick="window.open('https://forms.gle/naXsXmgUE7iTcH1V9','_blank');" class="uploadGFormButton" type="button" value="uploadGForm">
-      <input v-if="!checked" class="uploadGFormButton" type="button" value="uploadGForm">
+      <input v-if="checked" onclick="window.open('https://forms.gle/naXsXmgUE7iTcH1V9','_blank');" class="uploadGFormButton" type="button" value="Upload">
+      <input v-if="!checked" class="uploadGFormButton" type="button" value="Upload">
     </div>
   </div>
 </template>
