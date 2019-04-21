423723d9469887786189530130dd734a1faed085
Elisabeth Diana K S <elisabeth.dianaks@gmail.com>
2019-04-22 00:33:20 +0700

    [BAFE-21]
    - Set max width
    - Header and footer revamp
    - Delete motto
    - Comment menu
    - Fix search input

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="header">
-      <div class="header__logo">
-        <a href="/"><img src="../assets/img/S__17219904.jpg" alt="home"></a>
-      </div>
-      <div class="header__menu">
-        <the-menu></the-menu>
-      </div>
-      <div class="header__search">
-        <div class="search__input-group">
-          <input type="text" class="search__form-control" placeholder="Cari Disini" aria-label="Cari Disini" aria-describedby="basic-addon2">
-          <button class="search__input-group-append"><i class="fas fa-search"></i></button>
+  <div class="header__container">
+    <div class="container">
+      <div class="header">
+        <div class="header__logo">
+          <a href="/"><img src="../assets/img/logo-icon.png" alt="home"></a>
+        </div>
+        <div class="header__menu">
+          <h1>Citizen Journalism Platform for Innovative Solution</h1>
+          <!--<the-menu></the-menu>-->
+        </div>
+        <div class="header__search">
+          <div class="search__input-group">
+            <input type="text" class="search__form-control" placeholder="Cari Disini" aria-label="Cari Disini" aria-describedby="basic-addon2">
+            <button class="search__input-group-append"><i class="fas fa-search"></i></button>
+          </div>
+        </div>
+        <div class="header__action">
+          <button class="button button--round">Upload</button>
         </div>
       </div>
-      <div class="header__action">
-        <button class="button button--round">Upload</button>
-      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -27,47 +32,102 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import "../assets/style/scss/abstracts/variables";
   .header {
     display: flex;
+    box-sizing: border-box;
+    height: 65px;
+    padding: 10px 25px;
+    align-items: center;
+
+    &__container {
+      border-bottom: 1px solid $grey-2;
+    }
 
     &__logo img {
-      height: 100px;
+      height: 40px;
+    }
+
+    h1 {
+      color: $bafe;
+      font-size: 20px;
+      font-weight: 600;
+    }
+
+    &__menu {
+      margin: 0px 15px;
     }
 
     &__search {
       display: flex;
       align-items: center;
       margin-left: auto;
-      margin-right: 20px;
+      margin-right: 35px;
     }
 
     &__action {
       display: flex;
       align-items: center;
       justify-self: flex-end;
-      margin-right: 20px;
     }
   }
 
   .search {
     &__input-group {
+      position: relative;
       display: flex;
       height: 40px;
+      border-radius: 20px;
+      border: 1px solid $bafe;
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+      -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+      transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+      &::after {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        border-radius: 20px;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+        opacity: 0;
+        -webkit-transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+        transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+      }
+
+      &:hover {
+        -webkit-transform: scale(1.05, 1.05);
+        transform: scale(1.05, 1.05);
+      }
+
+      &:hover::after {
+        opacity: 1;
+      }
 
       &-append {
         padding: 0px 10px;
-        border: 2px solid #385446;
         border-radius: 0 20px 20px 0;
-        background-color: #385446;
-        color: #fafafa;
+        color: $bafe;
+        background-color: $white;
       }
     }
 
     &__form-control {
-      background-color: lightgrey;
-      padding: 0px 15px;
-      border: 2px solid #385446;
+      width: 100px;
+      background-color: $white;
+      padding: 0px 20px;
       border-radius: 20px 0 0 20px;
+
+      &:hover {
+        width: 200px;
+        -webkit-transition: width 0.6s ease-in-out;
+        -moz-transition: width 0.6s ease-in-out;
+        -o-transition: width 0.6s ease-in-out;
+        transition: width 0.6s ease-in-out;
+      }
     }
   }
 </style>
