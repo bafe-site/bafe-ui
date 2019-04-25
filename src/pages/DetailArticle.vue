15a53b26dcc3e113110f8bf4d88d617300174f6b
Elisabeth Diana K S <elisabeth.dianaks@gmail.com>
2019-04-25 21:14:11 +0700

    [BAFE-3]
    - Delete unused code
    - Fix content page

--- src/pages/DetailArticle.vue
+++ src/pages/DetailArticle.vue
@@ -1,41 +1,25 @@
 <template>
-  <!-- <div> -->
-    <div class="kontainer">
-      <div class="grid-item">
-        <div class="artikel">
-          <img class="gArtikel" :src="'data:image/jpeg;base64,'+ artikel.thumbnail"/>
-          <div class="kontainer-inside">
-            <div class="grid-item-inside" style="width: 150px; font-size: 80%; text-align: center;">
-              <img class="gPenulis" src="../assets/img/logo.jpg"/>
-              <span>{{ artikel.meta.author }}</span>
+    <div class="container">
+        <div class="article__container">
+          <img class="article__thumbnail" :src="'data:image/jpeg;base64,'+ artikel.thumbnail"/>
+          <div class="meta__container">
+            <div class="meta__category">
+              <span class="label">{{ artikel.category }} </span>
             </div>
-            <div class="grid-item-inside">
-              <span>View: {{ artikel.viewer }}</span>
-              <br>
-              <span>Created: {{artikel.meta.createdDate}}</span>
-            </div>
-          </div>
-          <h2>{{ artikel.title }}</h2>
-          <div>
-            <span class="kategori">Kategori: {{ artikel.category }} </span>
-          </div>
-          <div v-html="artikel.content">
-          </div>
-        </div>
-      </div>
-      <!-- <div class="grid-item">
-        <div>
-          <h2>Popular</h2>
-            </div>
-            <div class="flex-container" v-for="n in artikelPinggir.slice(0, 3)" :key="n.id">
-              <div>
-                <router-link :to="{ name: 'artikel', params: { id: n.id }}" :key="$route.path"><img class="gKontenPojok" :src="require('@/assets/img/' + n.thumbnail )"/></router-link>
-                <h5>{{ n.title }}</h5>
+            <div class="author__container">
+              <img class="author__profil-picture" src="../assets/img/logo.jpg"/>
+              <div class="author__info">
+                <div class="author__name">{{ artikel.meta.author }}</div>
+                <div>{{ artikel.meta.createdDate}}</div>
               </div>
             </div>
-        </div> -->
-    <!-- </div> -->
-  </div>
+          </div>
+          <div class="article__title">
+            <h2>{{ artikel.title }}</h2>
+          </div>
+          <div v-html="artikel.content"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -58,22 +42,10 @@ export default {
           self.artikel = res.data.content.data
           var d = new Date(self.artikel.meta.createdDate * 1000)
           self.artikel.meta.createdDate = d.toLocaleString()
-          // console.log(this.artikel)
-          // console.log(this.$route.params.id)
         })
         .catch(err => {
-          // console.log(Constant.article.getId.replace(/{id}/i, this.$route.params.id))
           console.log(err)
         })
-      // Axios
-      //   .get('http://localhost/bafe/public/api/article?orderBy=viewer&direction=desc')
-      //   .then(res => {
-      //     this.artikelPinggir = res.data.content.data
-      //     console.log(this.artikelPinggir)
-      //   })
-      //   .catch(err => {
-      //     console.log(err)
-      //   })
     }
   },
   mounted () {
@@ -85,64 +57,40 @@ export default {
 }
 </script>
 
-<style scoped>
-p {
-  border: 10px solid #ffffff;
-  margin: 0px 20px;
-  text-indent: .5in;
-}
+<style lang="scss" scoped>
+.article {
+  &__container {
+    display: block;
+    max-width: 750px;
+  }
 
-.flex-container {
-  display: flex;
-  flex-wrap: nowrap;
+  &__thumbnail {
+    width: 100%;
+  }
 }
+  .author {
+    &__container {
+      width: 300px;
+      display: block;
+      margin-top: 35px;
+      vertical-align: middle;
+    }
 
-.flex-container > div {
-  width: auto;
-  margin: 10px;
-  text-align: center;
-  line-height: 20px;
-  font-size: 30px;
-}
-.kontainer {
-  display: grid;
-  width: 100%;
-  grid-template-columns: 100%;
-  padding: 10px;
-}
-.kontainer-inside {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-.grid-item {
-  margin: 0 auto;
-  width: 65%;
-  font-size: 30px;
-  text-align: center;
-}
-.grid-item-inside {
-  width: 100%;
-  margin-top: 15px;
-}
-.gKontenPojok{
-  width: 400px;
-  height: 300px;
-}
-.artikel{
-  font-size: 18px;
-  text-align: justify;
-}
-.gArtikel{
-  width: 800px;
-  height: 600px;
-}
-.gPenulis{
-  width: 150px;
-  height: 150px;
-  padding-bottom: 0.5em;
-}
-.kategori {
-  font-size: 16px;
-  font-weight: lighter;
-}
+    &__name {
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    &__profil-picture {
+      width: 50px;
+      height: 50px;
+      float: left;
+      margin-right: 15px;
+    }
+  }
+
+  .container {
+    margin-top: 80px;
+    margin-bottom: 80px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,6 @@ export default new Router({
       path: '/article/:id',
       name: 'artikel',
       component: DetailArticle
-    },
+    }
   ]
 })
